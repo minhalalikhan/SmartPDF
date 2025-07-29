19606e53cd308f11b54de854fed13353ab5e34e7
Minhal Ali Khan <minhalalikhan.786@gmail.com>
2025-07-29 23:40:11 +0530

    added persist in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,16 @@ app.use(cors());
 app.use('/api', router)
 
 
+app.get("/persist", (req, res) => {
+  res.json({ message: "Server is running and ready to persist data." });
+});
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`File upload server listening on http://localhost:${PORT}`);
+
+  setInterval(() => {
+
+    fetch("https://smartpdf-a2ym.onrender.com/persist")
+  }, 12 * 60 * 1000);
 });
